3558132cf6183fb0c07b6e98096410dd2bf9849d
Distintiva Solutions <57623220+distintiva@users.noreply.github.com>
2020-02-12 19:28:13 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,34 @@
+function displayStoredScores () {
+    scores = arcadeDB.toLine("Number of scores: " + convertToText(arcadeDB.listCount("SCORES")))
+    for (let index = 0; index <= arcadeDB.listCount("SCORES") - 1; index++) {
+        score_at_index = arcadeDB.toLine(convertToText(arcadeDB.listGetValueAt("SCORES", index)))
+        scores = "" + scores + score_at_index
+    }
+    game.showLongText(scores, DialogLayout.Full)
+}
+function displayUserData () {
+    user_name = arcadeDB.getTextValue("NAME")
+    user_age = arcadeDB.getNumberValue("AGE")
+    game.showLongText(arcadeDB.toLines(["Player: " + user_name, "Age: " + user_age]), DialogLayout.Full)
+}
 function askNameAndAge () {
     user_name = game.askForString("Enter your player name")
     user_age = game.askForNumber("How old are you ?")
+    arcadeDB.setTextValue("NAME", user_name)
+    arcadeDB.setNumberValue("AGE", user_age)
 }
 let user_age = 0
 let user_name = ""
-if (game.ask("FOUND EXISTING DATABASE", "Do you want to delete it?")) {
-    askNameAndAge()
+let score_at_index = ""
+let scores = ""
+if (arcadeDB.exists() && game.ask("FOUND EXISTING DATABASE", "Do you want to delete it?")) {
+    arcadeDB.deleteAll()
 }
-game.splash("Player: " + user_name, "Age: " + user_age)
+if (!(arcadeDB.exists())) {
+    askNameAndAge()
+    arcadeDB.listAddValue("SCORES", 226)
+    arcadeDB.listAddValue("SCORES", 669)
+    arcadeDB.listAddValue("SCORES", 13)
+}
+displayUserData()
+displayStoredScores()
